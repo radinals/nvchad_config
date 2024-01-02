3c0ab2c26485ff7bd035c52497bbf5b497bef378
radinals <radinalsaragih@gmail.com>
2024-01-02 19:11:51 +0700

    added formatter configs

---
+++ configs/formatter.lua
@@ -0,0 +1,56 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+    cpp = {
+      function()
+        return {
+          exe = "clang-format",
+          args = {
+            "-assume-filename",
+            util.escape_path(util.get_current_buffer_file_name()),
+            "--style=InheritParentConfig",
+          },
+          stdin = true,
+          try_node_modules = true,
+        }
+      end
+    },
+    c = {
+      function ()
+        return {
+          exe = "clang-format",
+          args = {
+            "-assume-filename",
+            util.escape_path(util.get_current_buffer_file_name()),
+            "--style=InheritParentConfig",
+          },
+          stdin = true,
+          try_node_modules = true,
+        }
+      end
+    },
+    python = {
+      function ()
+        return {
+          exe = "autopep8",
+          args = {
+            "-",
+            -- "--max-line-length",
+            -- "80"
+          },
+          stdin = true,
+          try_node_modules = true,
+        }
+
+      end
+    }
+  }
+}
